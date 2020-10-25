5bc78e05939843a56da69bca04cea4140b7a1275
LZdziechowski <l.zdziechowski.contact@gmail.com>
2020-10-25 17:25:05 +0100

    6.2 My First Unit Tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,46 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("test test");
+    public static void main(String[] args) {
+
+        String testUsername = "testUsername";
+        String result;
+
+        SimpleUser simpleUser = new SimpleUser(testUsername);
+        result = simpleUser.getUsername();
+
+        if (result.equals(testUsername)) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        ///////////////////////////////////////////////
+
+        final int testNumber1 = 5;
+        final int testNumber2 = 7;
+        final int number1Addnumber2 = testNumber1 + testNumber2;
+        final int number1SubtractNumber2 = testNumber1 - testNumber2;
+        final int resultAddMethod;
+        final int resultSubtractMethod;
+
+        Calculator calculator = new Calculator();
+        resultAddMethod = calculator.add(testNumber1, testNumber2);
+        resultSubtractMethod = calculator.subtract(testNumber1, testNumber2);
+
+        if (resultAddMethod == number1Addnumber2) {
+            System.out.println("Add method OK");
+        } else {
+            System.out.println("Add method Error!");
+        }
+
+        if (resultSubtractMethod == number1SubtractNumber2) {
+            System.out.println("Subtract method OK");
+        } else {
+            System.out.println("Subtract method Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int add(int numberToAdd1, int numberToAdd2) {
+        return numberToAdd1 + numberToAdd2;
+    }
+    public int subtract(int numberToSubtract1, int numberToSubtract2) {
+        return numberToSubtract1 + numberToSubtract2;
+    }
+}
