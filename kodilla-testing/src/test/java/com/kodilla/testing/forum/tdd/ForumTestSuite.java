b79fdf66757eddcda9a4e76e223f78374ef93985
LZdziechowski <l.zdziechowski.contact@gmail.com>
2020-10-30 20:10:40 +0100

    6.4 in progress

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/tdd/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/tdd/ForumTestSuite.java
@@ -1,9 +1,8 @@
 package com.kodilla.testing.forum.tdd;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import com.kodilla.testing.forum.ForumPost;
+import com.kodilla.testing.forum.ForumUser;
+import org.junit.jupiter.api.*;
 
 @DisplayName("TDD: Forum Test Suite")
 public class ForumTestSuite {
@@ -25,4 +24,39 @@ public class ForumTestSuite {
         testCounter++;
         System.out.println("Preparing to execute test #" + testCounter);
     }
+
+    @DisplayName("when add post with a addPost() method, getPostQuantity() method return 1")
+    @Test
+    void testAddPost() {
+        System.out.println("Test Add Post.");
+        //Given
+        final String testName = "testName";
+        final String testRealName = "testRealName";
+        final String testPostBody = "testPostBody";
+        final ForumUser testForumUser = new ForumUser(testName, testRealName);
+        //When
+        testForumUser.addPost(testName, testPostBody);
+        //Then
+        Assertions.assertEquals(1, testForumUser.getPostQuantity());
+    }
+
+    @DisplayName("when add comment with a addComment() method, getCommentQuantity() method return 1")
+    @Test
+    void testAddComment() {
+        System.out.println("Test Add Comment");
+        //Given
+        final String testName = "testName";
+        final String testRealName = "testRealName";
+        final String testPostBody = "testPostBody";
+        final String testAuthor = "testAuthor";
+        final String testCommentBody = "testCommentBody";
+        final ForumUser testForumUser = new ForumUser(testName, testRealName);
+        final ForumPost testForumPost = new ForumPost(testPostBody, testAuthor);
+        //When
+        testForumUser.addComment(testForumPost, testAuthor,testCommentBody);
+        //Then
+        Assertions.assertEquals(1, testForumUser.getCommentQuantity());
+    }
+
+
 }
