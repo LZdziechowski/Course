7b0e27449d57fee27df9dd830d4340bf7aaab6c6
LZdziechowski <l.zdziechowski.contact@gmail.com>
2020-11-01 18:51:16 +0100

    6.4 in progress

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
@@ -17,41 +17,53 @@ public class ForumUser {
     }
 
     public void addPost(String author, String postBody) {
-        //do nothing
+        ForumPost forumPost = new ForumPost(postBody, author);
+        posts.add(forumPost);
     }
 
     public void addComment(ForumPost thePost, String author, String commentBody) {
-        //do nothing
+        ForumComment forumComment = new ForumComment(thePost, commentBody, author);
+        comments.add(forumComment);
     }
 
     public int getPostQuantity() {
-        //return 100 temporarily
-        return 100;
+        return posts.size();
     }
 
     public int getCommentQuantity() {
-        //return 100 temporarily
-        return 100;
+        return comments.size();
     }
 
     public ForumPost getPost(int postNumber) {
-        //return null means that the operation was unsuccessful
+        if(postNumber >= 0 && postNumber < posts.size()) {
+            return posts.get(postNumber);
+        }
         return null;
     }
 
     public ForumComment getComment(int commentNumber) {
-        //return null means that the operation was unsuccessful
+        if(commentNumber >= 0 && commentNumber < comments.size()) {
+            return comments.get(commentNumber);
+        }
         return null;
     }
 
     public boolean removePost(ForumPost thePost) {
-        //return true temporarily
-        return true;
+        boolean result = false;
+        if(posts.contains(thePost)) {
+            posts.remove(thePost);
+            result = true;
+        }
+        return result;
     }
 
     public boolean removeComment(ForumComment theComment) {
-        //return true temporarily
-        return true;
+        boolean result = false;
+        if(comments.contains(theComment)) {
+            comments.remove(theComment);
+            result = true;
+        }
+        return result;
     }
 
     public String getName() {
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/tdd/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/tdd/ForumTestSuite.java
@@ -5,7 +5,7 @@ import com.kodilla.testing.forum.ForumPost;
 import com.kodilla.testing.forum.ForumUser;
 import org.junit.jupiter.api.*;
 
-@DisplayName("TDD: Forum Test Suite")
+@DisplayName("TDD: Forum Test Suite \uD83D\uDE31")
 public class ForumTestSuite {
 
     private static int testCounter = 0;
@@ -72,8 +72,7 @@ public class ForumTestSuite {
         final ForumPost testForumPost = new ForumPost(testPostBody, testAuthor);
         testForumUser.addPost(testForumPost.getAuthor(), testForumPost.getPostBody());
         //When
-        final ForumPost retrivedPost;
-        retrivedPost = testForumUser.getPost(0);
+        final ForumPost retrivedPost = testForumUser.getPost(0);
         //Then
         Assertions.assertEquals(testForumPost, retrivedPost);
     }
@@ -93,8 +92,7 @@ public class ForumTestSuite {
         final ForumComment testForumComment = new ForumComment(testForumPost, testCommentBody, testAuthor);
         testForumUser.addComment(testForumPost, testForumComment.getAuthor(), testForumComment.getCommentBody());
         //When
-        final ForumComment retrivedComment;
-        retrivedComment = testForumUser.getComment(0);
+        final ForumComment retrivedComment = testForumUser.getComment(0);
         //Then
         Assertions.assertEquals(testForumComment, retrivedComment);
     }
@@ -111,7 +109,7 @@ public class ForumTestSuite {
         final ForumUser testForumUser = new ForumUser(testName, testRealName);
         final ForumPost testForumPost = new ForumPost(testPostBody, testAuthor);
         //When
-        boolean result = testForumUser.removePost(testForumPost);
+        final boolean result = testForumUser.removePost(testForumPost);
         //Then
         Assertions.assertFalse(result);
     }
@@ -130,8 +128,48 @@ public class ForumTestSuite {
         final ForumPost testForumPost = new ForumPost(testPostBody, testAuthor);
         final ForumComment testForumComment = new ForumComment(testForumPost, testCommentBody, testAuthor);
         //When
-        boolean result = testForumUser.removeComment(testForumComment);
+        final boolean result = testForumUser.removeComment(testForumComment);
         //Then
         Assertions.assertFalse(result);
+    }
+
+    @DisplayName("")
+    @Test
+    void testRemovePost() {
+        System.out.println("Test Remove Post");
+        //Given
+        final String testName = "testName";
+        final String testRealName = "testRealName";
+        final String testPostBody = "testPostBody";
+        final String testAuthor = "testAuthor";
+        final ForumUser testForumUser = new ForumUser(testName, testRealName);
+        final ForumPost testForumPost = new ForumPost(testPostBody, testAuthor);
+        testForumUser.addPost(testAuthor, testPostBody);
+        //When
+        final boolean result = testForumUser.removePost(testForumPost);
+        //Then
+        Assertions.assertTrue(result);
+        Assertions.assertEquals(0, testForumUser.getPostQuantity());
+    }
+
+    @DisplayName("")
+    @Test
+    void testRemoveComment() {
+        System.out.println("Test Remove Comment");
+        //Given
+        final String testName = "testName";
+        final String testRealName = "testRealName";
+        final String testPostBody = "testPostBody";
+        final String testAuthor = "testAuthor";
+        final String testCommentBody = "testCommentBody";
+        final ForumUser testForumUser = new ForumUser(testName, testRealName);
+        final ForumPost testForumPost = new ForumPost(testPostBody, testAuthor);
+        final ForumComment testForumComment = new ForumComment(testForumPost, testCommentBody, testAuthor);
+        testForumUser.addComment(testForumPost, testForumComment.getAuthor(), testForumComment.getCommentBody());
+        //When
+        final boolean result = testForumUser.removeComment(testForumComment);
+        //Then
+        Assertions.assertTrue(result);
+        Assertions.assertEquals(0, testForumUser.getCommentQuantity());
     }
 }
