b66f9756448d1deaf527ba9a21686179c9afc82c
LZdziechowski <l.zdziechowski.contact@gmail.com>
2020-11-21 14:02:11 +0100

    8.2 Adding exception.test package with FirstChallenge class.

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,12 +1,27 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.test.FirstChallenge;
+
+import java.io.File;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
+
+       /* final FileReader fileReader = new FileReader();
+        fileReader.readFile();*/
+
+
+        //--------------------  8.2  ---------------------//
+        final FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            final double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero!");
+        }
     }
 }
 
