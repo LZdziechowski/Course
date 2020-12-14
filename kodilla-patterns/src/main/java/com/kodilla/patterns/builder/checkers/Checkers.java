e18768e895d0a6c9eceef9242abcae607aa778e7
LZdziechowski <l.zdziechowski.contact@gmail.com>
2020-12-14 09:48:13 +0100

    11.5

--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/checkers/Checkers.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/checkers/Checkers.java
@@ -28,11 +28,55 @@ public class Checkers {
 
         private Board board = new Board();
         private String playerOne = "";
-        private String playerTwo =  "";
+        private String playerTwo = "";
 
-        public CheckerBuilder playerOne() {
-
+        public CheckerBuilder playerOne(String playerOne) {
+            this.playerOne = playerOne;
+            return this;
         }
 
+        public CheckerBuilder playerTwo(String playerTwo) {
+            this.playerTwo = playerTwo;
+            return this;
+        }
+
+        public CheckerBuilder figure(String figureType, String figureColor, int x, int y) {
+            if (x > Board.MAX_INDEX || x < Board.MIN_INDEX || y > Board.MAX_INDEX || y < Board.MIN_INDEX) {
+                throw new IllegalStateException("x and y should be in range between " + Board.MIN_INDEX + " and " + Board.MAX_INDEX);
+            }
+            if (board.getFigure(x, y) == null) {
+                board.setFigure(FigureFactory.makeFigure(figureType, figureColor), x, y);
+            } else {
+                throw new IllegalStateException("Position " + x + ", " + y + " is already occupied");
+            }
+            return this;
+        }
+
+        public Checkers build() {
+            if (playerOne.length() == 0) {
+                throw new IllegalStateException("There is no player One specified!");
+            }
+            if (playerTwo.length() == 0) {
+                throw new IllegalStateException("There is no player Two specified");
+            }
+            boolean white = false;
+            boolean black = false;
+            for (int x = Board.MIN_INDEX; x <= Board.MAX_INDEX; x++) {
+                for (int y = Board.MIN_INDEX; y <= Board.MAX_INDEX; y++) {
+                    Figure figure = board.getFigure(x, y);
+                    if (figure != null) {
+                        if (figure.getColor().equals(Figure.BLACK)) {
+                            black = true;
+                        } else {
+                            white = true;
+                        }
+                    }
+                }
+            }
+            if (!(black && white)) {
+                throw new IllegalStateException("There are no figures of both colors on the board!");
+            }
+            return new Checkers(board, playerOne, playerTwo);
+        }
     }
 }
