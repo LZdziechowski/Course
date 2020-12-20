5998e9c54e3cfbd12b9f7f1ab2d2d8ac369aad11
LZdziechowski <l.zdziechowski.contact@gmail.com>
2020-12-21 00:11:50 +0100

    13.3 in progress

---
+++ hibernate/src/test/java/com/hibernate/task/dao/TaskFinancialDetailsDao.java
@@ -0,0 +1,25 @@
+package com.hibernate.task.dao;
+
+import com.hibernate.task.TaskFinancialDetails;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TaskFinancialDetailsDao {
+
+    @Autowired
+    private TaskFinancialDetailsDao taskFinancialDetailsDao;
+
+    @Test
+    void testFindByPaid() {
+        //Given
+        TaskFinancialDetails taskFinancialDetails = new TaskFinancialDetails(new BigDecimal(115), false);
+        taskFinancialDetailsDao.sa
+    }
+}
